b5814e57fd245a9fc8c3cc665f2774a36df47563
Fredrik Lillejordet <fretholil@gmail.com>
2015-10-08 14:26:45 +0200

    fjernet gamle filer og la til hello world node.

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-'use strict';
+// Load the http module to create an http server.
+var http = require('http');
 
-var React = require('react');
-
-var ExampleApplication = React.createClass({
-    render: function() {
-        var elapsed = Math.round(this.props.elapsed / 100);
-        var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0');
-        var message =
-            'React has been successfully running for ' + seconds + ' seconds.';
-
-        return <p > {
-            message
-        } < /p>;
-    }
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.end("Hello World\n");
 });
 
-var start = new Date().getTime();
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
 
-setInterval(function() {
-    React.render( < ExampleApplication elapsed = {
-            new Date().getTime() - start
-        }
-        />,
-        document.getElementById('container')
-    );
-}, 50);
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
+
+
+/*
+Old example ping output: 
+Wed Sep 30 12:29:24 2015 64 bytes from 10.0.30.22: icmp_seq=2 ttl=126 time=43.8 ms
+*/
