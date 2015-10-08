c0c6bde423ef93accf26f4d51cb13b50ce9ff3b5
Fredrik Lillejordet <fretholil@gmail.com>
2015-10-08 14:30:03 +0200

    Flyttet gammel eksempel ping til egen fil

--- app.js
+++ app.js
@@ -9,8 +9,3 @@ var server = http.createServer(function(request, response) {
 
 server.listen(8000);
 console.log("Server running at http://127.0.0.1:8000/");
-
-/*
-Old example ping output: 
-Wed Sep 30 12:29:24 2015 64 bytes from 10.0.30.22: icmp_seq=2 ttl=126 time=43.8 ms
-*/
