9d5af49f7ee7045b3ccfc78255f479cde2b4b6ee
Fredrik Lillejordet <fretholil@gmail.com>
2015-10-09 13:28:05 +0200

    refaktor, fungerer i docker

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var util = require('util');
 var exec = require('child_process').exec;
 var moment = require('moment');
 var Datastore = require('nedb');
-var db = new Datastore({ filename: 'pings.db', autoload: true });
+var db = new Datastore({
+    filename: 'pings.db',
+    autoload: true
+});
 
 var dateRegex = /\w{3}\s+[0-9]{1,2}\s+[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\s+[0-9]{4}/;
 var pingTimeRegex = /.*?time=(.*?ms)/;
@@ -15,93 +18,99 @@ var PING_INTERVAL = 1000;
 
 // Ping and NeDB
 function handlePingOutput(error, stdout, stderr) {
-	if (error || stdout.match(/DUP/)) {
-		return;
-	}
-	
-	try {
-		var line = stdout.split("\n")[1];
-		var date = line.match(dateRegex)[0];
-		var dateInUnixTime = moment(date, 'MMM DD hh:mm:ss YYYY').unix();
-		var ip = line.match(ipRegex)[0];
-		var ping = Number(line.match(pingTimeRegex)[1].split(" ")[0]);
-	
-		var pingObjToSave = {
-			"date": date,
-			"unixtime": (dateInUnixTime * 1000),
-			"ip": ip,
-			"ping": ping
-		};
-	
-		savePingObjToDatabase(pingObjToSave);
-	} catch (err) {
-		console.log(err);
-	}
+    if (error || stdout.match(/DUP/)) {
+        return;
+    }
+
+    try {
+        var line = stdout.split("\n")[1];
+        var date = line.match(dateRegex)[0];
+        var dateInUnixTime = moment(date, 'MMM DD hh:mm:ss YYYY').unix();
+        var ip = line.match(ipRegex)[0];
+        var ping = Number(line.match(pingTimeRegex)[1].split(" ")[0]);
+
+        var pingObjToSave = {
+            "date": date,
+            "unixtime": (dateInUnixTime * 1000),
+            "ip": ip,
+            "ping": ping
+        };
+
+        savePingObjToDatabase(pingObjToSave);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 
 var savePingObjToDatabase = function(objToSave) {
-	db.insert(objToSave, function(err, newDoc) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log("Ping saved to NeDB");
-	});
+    db.insert(objToSave, function(err, newDoc) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Ping saved to NeDB");
+    });
 };
 
 
 setInterval(function() {
-	exec("ping -c 1 " + PING_ADDR + " | perl -nle 'BEGIN {$|++} print scalar(localtime), \" \", $_' ", handlePingOutput);
+    exec("ping -c 1 " + PING_ADDR + " | perl -nle 'BEGIN {$|++} print scalar(localtime), \" \", $_' ", handlePingOutput);
 }, PING_INTERVAL);
 
 function compare(a, b) {
-	if (a.unixtime < b.unixtime) {
-		return -1;
-	}
-	if (a.unixtime > b.unixtime) {
-		return 1;
-	}
-	return 0;
+    if (a.unixtime < b.unixtime) {
+        return -1;
+    }
+    if (a.unixtime > b.unixtime) {
+        return 1;
+    }
+    return 0;
 }
 
 
 // Express API
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
 app.get('/data', function(request, response) {
-	db.find({}, function (err, docs) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		docs.sort(compare);
-		response.end(JSON.stringify(docs));
-	});
+    db.find({}, function(err, docs) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        docs.sort(compare);
+        response.end(JSON.stringify(docs));
+    });
 });
 
 app.get('/last24hours', function(request, response) {
-	var oneDayAgo = moment().subtract(1, 'days').unix() * 1000;
-	db.find({"unixtime": {$gt: oneDayAgo}}, function(err, docs) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		docs.sort(compare);
-		response.end(JSON.stringify(docs));
-	});
+    var oneDayAgo = moment().subtract(1, 'days').unix() * 1000;
+    db.find({
+        "unixtime": {
+            $gt: oneDayAgo
+        }
+    }, function(err, docs) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        docs.sort(compare);
+        response.end(JSON.stringify(docs));
+    });
 });
 
 app.get('/delete', function(request, response) {
-	if (request.query.secret === process.env.DELETE_SECRET) {
-		db.remove({}, {multi: true}, function (err, numRemoved) {
-			console.log(numRemoved + " entries deleted.")
-		});
-	}
+    if (request.query.secret === process.env.DELETE_SECRET) {
+        db.remove({}, {
+            multi: true
+        }, function(err, numRemoved) {
+            console.log(numRemoved + " entries deleted.")
+        });
+    }
 });
 
 app.listen(5000);
