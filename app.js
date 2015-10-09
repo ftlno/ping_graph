4263957f08326f910c12fb8d14657490a0c27d25
Fredrik Lillejordet <fretholil@gmail.com>
2015-10-09 07:19:25 +0200

    la til en spørring mot node fra html

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/data', function(request, response) {
+    var data = {
+        'url': "http://db.no",
+        'time': '12345',
+        'ms': '10'
+    };
+    response.json(data);
+});
+
+
 app.listen(3000);
